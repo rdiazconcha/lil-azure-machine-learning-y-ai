dfedfe37ec040cd3c122d02794ad6a3702aa8876
booth06-mgr2 <booth06-mgr2@content.linkedin.biz>
2019-04-26 18:24:31 +0200

    Implementación de clase ApiKeyServiceClientCredentials

---
+++ modulo-7/textos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+
+namespace textos
+{
+    class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        private string key;
+        public ApiKeyServiceClientCredentials(string key)
+        {
+            this.key = key;
+        }
+
+        public override Task ProcessHttpRequestAsync(System.Net.Http.HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
