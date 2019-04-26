a324bcfbde1a197b84746a8d2ec2d55d5290b4f7
booth06-mgr2 <booth06-mgr2@content.linkedin.biz>
2019-04-26 15:14:36 +0200

    Detección de textos y gitignore

--- modulo-5/vision/Program.cs
+++ modulo-5/vision/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
@@ -43,6 +44,54 @@ namespace vision
                     System.Console.WriteLine();
                 }
             }
+
+            files = Directory.GetFiles(Environment.CurrentDirectory, "texto*.jpg");
+
+            foreach (var file in files)
+            {
+
+                using (Stream stream = File.OpenRead(file))
+                {
+
+                    var analysis = client.BatchReadFileInStreamAsync(stream, TextRecognitionMode.Printed).Result;
+
+                    string operationId = analysis.OperationLocation.Substring(analysis.OperationLocation.Length - 36);
+
+                    var result = client.GetReadOperationResultAsync(operationId).Result;
+
+                    while (result.Status == TextOperationStatusCodes.Running ||
+                    result.Status == TextOperationStatusCodes.NotStarted)
+                    {
+                        System.Console.WriteLine("Esperando...");
+                        Task.Delay(1000).Wait();
+
+                        result = client.GetReadOperationResultAsync(operationId).Result;
+
+                    }
+
+
+                    if (result.RecognitionResults == null)
+                    {
+                        continue;
+                    }
+
+                    foreach ( var recognition in result.RecognitionResults)
+                    {
+
+                        foreach (var line in recognition?.Lines)
+                        {
+                            System.Console.WriteLine(line.Text);
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+
 
 
 
