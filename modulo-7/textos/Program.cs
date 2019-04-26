d637b0b6e697677bbb08ea71717e54235360780a
booth06-mgr2 <booth06-mgr2@content.linkedin.biz>
2019-04-26 18:56:50 +0200

    Detección de idioma

--- modulo-7/textos/Program.cs
+++ modulo-7/textos/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 using Microsoft.Rest;
 
 namespace textos
@@ -23,7 +27,35 @@ namespace textos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args==null || !args.Any())
+            {
+                System.Console.WriteLine("Por favor indicar el texto");
+                return;
+            }
+
+            var text = args[0];
+
+
+            string endpoint = "https://westeurope.api.cognitive.microsoft.com/";
+            string key = "c50bac85176f4da384d8e2d06bf17d41";
+
+            var credentials = new ApiKeyServiceClientCredentials(key);
+            var client = new TextAnalyticsClient(credentials);
+            client.Endpoint = endpoint;
+
+
+            var input = new List<LanguageInput>();
+            input.Add(new LanguageInput(id: "1", text: text));
+            var batch = new LanguageBatchInput(input);
+            var analysis   = client.DetectLanguageAsync(false, batch).Result;
+
+            foreach (var document in analysis.Documents)
+            {
+                var language = document.DetectedLanguages.FirstOrDefault();
+                System.Console.WriteLine(language.Iso6391Name);
+            }
+
+
         }
     }
 }
